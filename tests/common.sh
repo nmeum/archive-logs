bb4f513ca27786f1266f2dd46d144397f4107a37
Sören Tempel <soeren+git@soeren-tempel.net>
2020-06-12 15:26:39 +0200

    tests: use unified diff format

--- tests/common.sh
+++ tests/common.sh
@@ -1,5 +1,5 @@
 compare_dirs() {
-	diffout="$(diff -r "${1}" "${2}")"
+	diffout="$(diff -ur "${1}" "${2}")"
 	if [ $? -ne 0 ]; then
 		printf "FAIL: Directories differ.\n\n%s\n" "${diffout}"
 		exit 1
